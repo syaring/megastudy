48648ee1d14de18bb7c63df499b5c8b29c2fa377
syaring <sarah.k.serim@gmail.com>
2025-02-18 12:25:26 +0900

    주제추천페이지 > 심화탐구 키워드 선택 페이지

--- src/app/recommend/page2/page.tsx
+++ src/app/recommend/page2/page.tsx
@@ -1,20 +1,82 @@
 "use client"
 
 import { useRouter } from 'next/navigation';
-import { Button } from 'antd';
+import { Button, Layout, Tag } from 'antd';
+import { useState } from 'react';
+
+const { Header, Content, Footer } = Layout;
 
 export default function Page () {
-  const router = useRouter();
+  const router = useRouter()
+  const [selectedKeywords, setSelectedKeywords] = useState<string[]>([]);
 
-  const onClick = () => {
-    router.push('/recommend/page1');
-  }
+  const keywordList = [
+    { name: '면역', key: 'immunity' },
+    { name: '당뇨병', key: 'diabetes' },
+    { name: '암', key: 'cancer' },
+    { name: '정신병', key: 'psychosis' },
+    { name: '세포', key: 'cell' },
+  ];
+
+  const onClickNext = () => {
+    const isFullySelected = selectedKeywords.length === 3;
+
+    if (!isFullySelected) {
+      alert('내 활동 키워드를 3개 선택해주세요.');
+
+      return;
+    }
+
+    router.push('/recommend/page3');
+  };
+
+  const onChangeKeywordTag = (key: string, checked: boolean) => {
+    // checked
+    // true = 선택되지 않은 값이 선택됨
+    // false = 선택된 값이 해제됨
+
+    const isFullySelected = selectedKeywords.length === 3;
+
+    if (isFullySelected && checked) {
+      // 3개가 이미 선택된 상태에서 추가로 선택하려고 할 경우
+      return;
+    }
+
+    const nextSelectedKeywords = checked
+      ? [...selectedKeywords, key]
+      : selectedKeywords.filter((keyword) => keyword !== key);
+
+    setSelectedKeywords(nextSelectedKeywords);
+  };
+
   return (
-    <div>
-      추천서비스 2
-      <Button onClick={onClick}>
-        이전으로
-      </Button>
-    </div>
+    <Layout>
+      <Header style={{ color: "white" }}>
+        주제 추천 시작하기
+      </Header>
+
+      <Content>
+        <div>
+          AI 분석으로 학생의 생기부에서 추출한 5가지 활동 키워드입니다. <br />
+          이 중 학생이 생각하는 중요 키워드 3가지를 고르세요.
+        </div>
+
+        {keywordList.map(({ name, key }) => (
+          <Tag.CheckableTag
+            key={key}
+            checked={selectedKeywords.includes(key)}
+            onChange={(checked) => onChangeKeywordTag(key, checked)}
+          >
+            {name}
+          </Tag.CheckableTag>
+        ))}
+      </Content>
+
+      <Footer>
+        <Button onClick={onClickNext} type="primary">
+          다음
+        </Button>
+      </Footer>
+    </Layout>
   );
 }
