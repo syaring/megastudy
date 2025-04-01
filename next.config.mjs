589a60dc16e3506a714d4d2c49556b2fdd6400e3
syaring <sarah.k.serim@gmail.com>
2025-04-01 14:15:47 +0900

    api endpoint 변경

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
-    apiDemo: 'https://4vdphvayzg.execute-api.ap-northeast-2.amazonaws.com/demo',
+    apiDemo: 'https://rt7vn8bn2l.execute-api.ap-northeast-2.amazonaws.com/demo',
   },
 };
 
